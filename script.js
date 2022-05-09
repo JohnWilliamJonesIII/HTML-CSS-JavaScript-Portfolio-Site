9088ad728a9348d038c686b7b3712011c51a682a
John William Jones III <johnwilliamjonesiii@gmail.com>
2022-05-08 20:06:06 -0500

    Added styling for upcoming Light/Dark Mode functionality, removed horizontal scrolling

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/* Light/Dark Mode Button Scripting */
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
